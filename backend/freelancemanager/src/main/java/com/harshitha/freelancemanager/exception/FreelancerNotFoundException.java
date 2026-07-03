fc620b6c925dafdb3aee4e7094c3557ec376c98a
harshitha-chittoor <harshikarthi1606@gmail.com>
2026-07-03 11:21:41 +0530

    Remove old backend

--- backend/freelancemanager/src/main/java/com/harshitha/freelancemanager/exception/FreelancerNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.harshitha.freelancemanager.exception;
-
-public class FreelancerNotFoundException extends RuntimeException {
-
-    public FreelancerNotFoundException(String message) {
-        super(message);
-    }
-}
