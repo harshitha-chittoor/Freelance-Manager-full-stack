4f6c18211187a26e3f1b7a3191ab8cedaaf1c8e4
harshitha-chittoor <harshikarthi1606@gmail.com>
2026-07-03 11:24:51 +0530

    Remove incorrect backend (cloned version)

--- backend/freelancemanager/src/main/java/com/harshitha/freelancemanager/exception/ProposalNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.harshitha.freelancemanager.exception;
-
-public class ProposalNotFoundException extends RuntimeException {
-
-    public ProposalNotFoundException(String message) {
-        super(message);
-    }
-}
